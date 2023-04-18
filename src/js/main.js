9519b0b8d4c580e2e982b66f0ba534118e7693aa
Madelyn Arana <madelynarana@gmail.com>
2023-04-18 16:48:31 -0600

    Añadir efectos y sonidos.

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,32 @@
 
 const narutoCharacter = document.getElementById('naruto-character');
-
+    
 let xAxis = 0,
-    maxleft = 200;
+    maxleft = 500,
+    minLeft = 50;
 
+
+const AddClassAndSound = ( nameClass, soundById ) => {
+    
+    narutoCharacter.classList.add( nameClass );
+    document.querySelector( soundById ).play();
+    
+}
 
 const gamayu_endan = () => {
-
-    narutoCharacter.classList.add('gamayu_endan');
+    
+    AddClassAndSound('gamayu_endan', '#gamayu-sound');
 
     setTimeout(() => {
         narutoCharacter.classList.remove('gamayu_endan');
     }, 9000);
+
 }
 
 const rasengan = () => {
-
-    narutoCharacter.classList.add('rasengan');
     
+    AddClassAndSound('rasengan', '#rasengan-sound');
+
     setTimeout(() => {
         narutoCharacter.classList.remove('rasengan');
     }, 3000);
@@ -27,34 +36,68 @@ const run = () => {
 
     xAxis += 10;
 
-    narutoCharacter.classList.add('run');    
+    AddClassAndSound('run', '#run-sound');
 
-    if (xAxis < maxleft) narutoCharacter.style.left = `${xAxis}px`;
-    
+    if (xAxis <= maxleft) narutoCharacter.style.left = `${xAxis}px`
+
+}
+
+const back = () => {
+
+    xAxis -= 2;
+
+    narutoCharacter.classList.add('jump');
+
+    AddClassAndSound('jump', '#jump-sound');
+
+    if (xAxis > minLeft) narutoCharacter.style.left = `${xAxis}px`;
+
 }
 
 
-const keyboardActions = {
-    
-    ArrowDown: () => {
-        narutoCharacter.classList.add('crouch');
-    },
-    ArrowRight: () => run(),
+const crouch = () => {
 
-    KeyZ: () => gamayu_endan(),
-    KeyX: () => rasengan(),
-    
-    default: () => {
-        console.log('Selecciona una tecla válida.');
+    AddClassAndSound('crouch', '#crouch-sound');
+}
+
+const jump = () => {
+
+    AddClassAndSound('jump', '#jump-sound');
+
+}
+
+const rasenshuriken = () => {
+
+    AddClassAndSound('rasenshuriken', '#rasenshuriken-sound');
+
+    setTimeout(() => {
+
+        narutoCharacter.classList.remove('rasenshuriken');
+
+    }, 5000);
+
+}
+
+const keyboardActions = {
+
+    ArrowUp:    () => jump(),
+    ArrowDown:  () => crouch(),
+    ArrowRight: () => run(),
+    ArrowLeft:  () => back(),
+    KeyZ:       () => gamayu_endan(),
+    KeyX:       () => rasengan(),
+    KeyC:       () => rasenshuriken(),
+    default:    () => {
+        console.log('Presione una tecla válida.');
     }
 
 };
 
 
-addEventListener("keydown", function ( e ) {
-    
-    console.log( e.code );
-    
+addEventListener("keydown", function (e) {
+
+    console.log(e.code);
+
     const action = keyboardActions[e.code] || keyboardActions.default;
 
     action();
@@ -62,8 +105,8 @@ addEventListener("keydown", function ( e ) {
 });
 
 
-document.addEventListener('keyup', function ( e ) {
-    
-    narutoCharacter.classList.remove('crouch','run');
+document.addEventListener('keyup', function (e) {
+
+    narutoCharacter.classList.remove('crouch', 'run', 'jump');
 
 })
