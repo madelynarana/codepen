a0e74dd8a40ec23cc1ebf05d9e10739e2cbc9d79
Madelyn Arana <madelynarana@gmail.com>
2023-04-18 00:14:31 -0600

    Añadir efecto gamayu endan y rasengan.

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,42 @@
 const narutoCharacter = document.getElementById('naruto-character');
 
 
+
+
+const gamayu_endan = () => {
+
+    narutoCharacter.classList.add('gamayu_endan');
+
+    setTimeout(() => {
+        narutoCharacter.classList.remove('gamayu_endan');
+    }, 9000);
+}
+
+const rasengan = () => {
+
+    narutoCharacter.classList.add('rasengan');
+    
+    setTimeout(() => {
+        narutoCharacter.classList.remove('rasengan');
+    }, 3000);
+}
+
+const run = () => {
+    console.log('Corriendo');
+
+    narutoCharacter.classList.add('run');
+    
+}
+
+
 const keyboardActions = {
     
     ArrowDown: () => {
         narutoCharacter.classList.add('crouch');
     },
+
+    KeyZ: () => gamayu_endan(),
+    KeyX: () => rasengan(),
     
     default: () => {
         console.log('Selecciona una tecla válida.');
@@ -16,6 +47,8 @@ const keyboardActions = {
 
 
 addEventListener("keydown", function ( e ) {
+    
+    console.log( e.code );
     
     const action = keyboardActions[e.code] || keyboardActions.default;
 
